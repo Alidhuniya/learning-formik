7653c6a9aae93b90b45fc46e8689640f584899e4
alidhuniya <alihussaindhuniya0307@gmail.com>
2021-05-31 17:47:52 +0545

    3. multiple input filed

--- src/index.js
+++ src/index.js
@@ -4,14 +4,40 @@ import { useFormik } from "formik";
 import "./styles.css";
 
 const SignupForm = () => {
+  // Note that we have to initialize ALL of fields with values. These
+  // could come from props, but since we don’t want to prefill this form,
+  // we just use an empty string. If we don’t do this, React will yell
+  // at us.
   const formik = useFormik({
-    initialValues: { email: "" },
+    initialValues: {
+      firstName: '',
+      lastName: '',
+      email: '',
+    },
     onSubmit: values => {
       console.log(values)
-    }
+    },
   });
   return (
     <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="firstName">First Name</label>
+      <input
+        id="firstName"
+        name="firstName"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.firstName}
+      />
+
+      <label htmlFor="lastName">Last Name</label>
+      <input
+        id="lastName"
+        name="lastName"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.lastName}
+      />
+
       <label htmlFor="email">Email Address</label>
       <input
         id="email"
@@ -20,6 +46,7 @@ const SignupForm = () => {
         onChange={formik.handleChange}
         value={formik.values.email}
       />
+
       <button type="submit">Submit</button>
     </form>
   );
