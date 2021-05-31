cefcd456d743bb78a813ad6c60a9f16df8414d38
alidhuniya <alihussaindhuniya0307@gmail.com>
2021-05-31 17:32:22 +0545

    2.starter code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { useFormik } from "formik";
+import "./styles.css";
 
+const SignupForm = () => {
+  const formik = useFormik({
+    initialValues: { email: "" },
+    onSubmit: values => {
+      console.log(values)
+    }
+  });
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="email">Email Address</label>
+      <input
+        id="email"
+        name="email"
+        type="email"
+        onChange={formik.handleChange}
+        value={formik.values.email}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function App() {
+  return <SignupForm />;
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<React.StrictMode><App /></React.StrictMode>, rootElement);
