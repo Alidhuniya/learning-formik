ff5bf9454b8039b7dcf1cdeab74e82d4b3605567
alidhuniya <alihussaindhuniya0307@gmail.com>
2021-06-01 08:32:13 +0545

    5. yup-validationschema object

--- src/index.js
+++ src/index.js
@@ -2,44 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { useFormik } from "formik";
 import "./styles.css";
+import * as Yup from 'yup';
 
-// A custom validation function. This must return an object
- // which keys are symmetrical to our values/initialValues
- const validate = values => {
-  const errors = {};
-  if (!values.firstName) {
-    errors.firstName = 'Required';
-  } else if (values.firstName.length > 15) {
-    errors.firstName = 'Must be 15 characters or less';
-  }
-
-  if (!values.lastName) {
-    errors.lastName = 'Required';
-  } else if (values.lastName.length > 20) {
-    errors.lastName = 'Must be 20 characters or less';
-  }
-
-  if (!values.email) {
-    errors.email = 'Required';
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = 'Invalid email address';
-  }
-
-  return errors;
-};
 
 const SignupForm = () => {
-  // Note that we have to initialize ALL of fields with values. These
-  // could come from props, but since we don’t want to prefill this form,
-  // we just use an empty string. If we don’t do this, React will yell
-  // at us.
+ 
   const formik = useFormik({
     initialValues: {
       firstName: '',
       lastName: '',
       email: '',
     },
-    validate,
+    validationSchema: Yup.object({
+      firstName: Yup.string()
+        .max(15, 'Must be 15 characters or less')
+        .required('Required'),
+      lastName: Yup.string()
+        .max(20, 'Must be 20 characters or less')
+        .required('Required'),
+      email: Yup.string().email('Invalid email address').required('Required'),
+    }),
     onSubmit: values => {
       console.log(values)
     },
